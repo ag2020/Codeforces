5787c1a8632af44581be3641a7a39186c8a184a0
ag2020 <33589152+ag2020@users.noreply.github.com>
2018-03-24 12:24:32 -0700

    Create ProblemOne.java

---
+++ Round 472 - Div. 2/ProblemOne.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ProblemOne {
+
+	public static void main(String[] args) {
+		run();
+	}
+	
+	public static void run() {
+		InputReader2 ir = new InputReader2(System.in);
+		int n = ir.nextInt();
+		String s = ir.next();
+		for (int i = 0; i < n - 1; i++) {
+			if (s.charAt(i) == s.charAt(i + 1) && s.charAt(i) != '?') {
+				System.out.println("No");
+				return;
+			}
+		}
+		
+		int i = 0;
+		while (i < n) {
+			if (s.charAt(i) == '?' && (i == 0 || i == n - 1 || (s.charAt(i + 1) == '?' || s.charAt(i - 1) == s.charAt(i + 1)))) {
+				System.out.println("Yes");
+				return;
+			}
+			i++;
+		}
+		
+		System.out.println("No");
+	}
+}
+
+class InputReader2 {
+	
+	public BufferedReader br;
+    public StringTokenizer st;
+    
+	public InputReader2(InputStream is) {
+        br = new BufferedReader(new InputStreamReader(is), 32768);
+        st = null;
+    }
+	
+	public String next() {
+        while (st == null || !st.hasMoreTokens()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return st.nextToken();
+    }
+	
+	public int nextInt() {
+        return Integer.parseInt(next());
+    }
+}
